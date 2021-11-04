ef4f1a41a1fe1a7fa36ad9a23b7d4bc3b7489257
Salvydas Lukosius <sall@w-ss.io>
2021-11-04 01:25:13 +0000

    :white_source_mark:

--- zsh-lint.plugin.zsh
+++ zsh-lint.plugin.zsh
@@ -5,7 +5,7 @@
 # License MIT
 
 # Handle $0 according to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# http://z-shell.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
